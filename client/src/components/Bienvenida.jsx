40b735edea93c1275210791eaa47fd2f51239fdc
Samuel Aragon <samuelaragon@Air-de-Samuel.fibertel.com.ar>
2023-10-21 15:15:04 -0300

    client

---
+++ client/src/components/Bienvenida.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Bienvenida = () => {
+  return (
+    <div>
+      <h1>¡Bienvenidos al sitio mas grande de F1!</h1>
+      <button>INGRESAR</button>
+    </div>
+  );
+};
+
+export default Bienvenida;
